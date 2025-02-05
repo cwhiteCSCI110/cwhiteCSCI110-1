e1d15df1e6d6d3be8544c64fb3cc7257ce93991b
cwhiteCSCI110 <cawhite@mavs.coloradomesa.edu>
2025-02-04 19:51:22 -0700

    Add files via upload

---
+++ cwhite_2.14problem1.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------------------------
+# Name:        Section 2.14 Problem 1
+# Purpose:
+#
+# Author:      ClayWhite
+#
+# Created:     02/04/2025
+# Copyright:   (c) white 2025
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+everything = "All"  #assigned the values on the right to the variables on the left using the assignment token
+job = " work"
+conjunction = " and"
+deny = " no"
+game = " play"
+verb = " makes"
+name = " Jack"
+article = " a"
+boring = " dull"
+gender = " boy."
+print(everything + job + conjunction + deny + game + verb + name + article + boring + gender)
